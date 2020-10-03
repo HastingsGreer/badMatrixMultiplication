1f0eaa0dbad9ba720ccbfc99dacd22b7109aa1ee
Hastings Greer <hastings@hgreer.com>
2020-10-03 15:06:33 -0400

    better?

--- ab_matrix.pl
+++ ab_matrix.pl
@@ -28,6 +28,12 @@ addrow([HA|TA], [HB|TB], [HC|TC]):-
 nn(NN) :-
     NN #= 4.
 
+identity(I):-
+	I = [[1, 0, 0, 0],
+	      [0, 1, 0, 0],
+	      [0, 0, 1, 0],
+	      [0, 0, 0, 1]].
+
 isMat([], 0).
 isMat([H|T], N):-
     nn(NN),
@@ -41,11 +47,57 @@ isRow([H|T], N):-
     isRow(T, Ns),
     Ns #= N - 1.
 
+
+smolMat([], 0).
+smolMat([H|T], N):-
+    nn(NN),
+    smolRow(H, NN),
+    smolMat(T, Ns),
+    Ns #= N - 1.
+
+smolRow([], 0).
+smolRow([H|T], N):-
+    H in 1..4,
+    smolRow(T, Ns),
+    Ns #= N - 1.
+
+
 det([[A1, A2], [A3, A4]], 2, D):-
     D #= A1 * A4 - A2 * A3.
 
+
+det([[E_0, E_1, E_2], [E_3, E_4, E_5], [E_6, E_7, E_8]], 3, D):-
+    D #= -(E_5*E_7 - E_4*E_8)*E_0 + (E_2*E_7 - E_1*E_8)*E_3 - (E_2*E_4 - E_1*E_5)*E_6.
+
+det([[E_0, E_1, E_2, E_3],
+ [E_4, E_5, E_6, E_7],
+  [E_8, E_9, E_10, E_11],
+   [E_12, E_13, E_14, E_15]], 4, D):-
+	   D #= E_1*E_11*E_14*E_4 - E_1*E_10*E_15*E_4 - E_11*E_13*E_2*E_4 + E_10*E_13*E_3*E_4 - E_0*E_11*E_14*E_5 + E_0*E_10*E_15*E_5 + E_11*E_12*E_2*E_5 - E_10*E_12*E_3*E_5 - E_1*E_11*E_12*E_6 + E_0*E_11*E_13*E_6 + E_1*E_10*E_12*E_7 - E_0*E_10*E_13*E_7 - E_15*E_2*E_5*E_8 + E_14*E_3*E_5*E_8 + E_1*E_15*E_6*E_8 - E_13*E_3*E_6*E_8 - E_1*E_14*E_7*E_8 + E_13*E_2*E_7*E_8 + E_15*E_2*E_4*E_9 - E_14*E_3*E_4*E_9 - E_0*E_15*E_6*E_9 + E_12*E_3*E_6*E_9 + E_0*E_14*E_7*E_9 - E_12*E_2*E_7*E_9.
 %minor(MM, I, J, Mm):-
     
+cofactors([[E_0, E_1, E_2, E_3],
+   [E_4, E_5, E_6, E_7],
+   [E_8, E_9, E_10, E_11],
+   [E_12, E_13, E_14, E_15]], 4, C):-
+
+ maplist(maplist(#=), C, [[E_11*E_13*E_6 - E_10*E_13*E_7 - (E_11*E_14 - E_10*E_15)*E_5 - (E_15*E_6 - E_14*E_7)*E_9,
+  E_1*E_11*E_14 - E_1*E_10*E_15 - E_11*E_13*E_2 + E_10*E_13*E_3 + (E_15*E_2 - E_14*E_3)*E_9,
+  -(E_15*E_2 - E_14*E_3)*E_5 + (E_1*E_15 - E_13*E_3)*E_6 - (E_1*E_14 - E_13*E_2)*E_7,
+  -E_1*E_11*E_6 + E_1*E_10*E_7 + (E_11*E_2 - E_10*E_3)*E_5 + (E_3*E_6 - E_2*E_7)*E_9],
+ [-E_11*E_12*E_6 + E_10*E_12*E_7 + (E_11*E_14 - E_10*E_15)*E_4 + (E_15*E_6 - E_14*E_7)*E_8,
+  -E_0*E_11*E_14 + E_0*E_10*E_15 + E_11*E_12*E_2 - E_10*E_12*E_3 - (E_15*E_2 - E_14*E_3)*E_8,
+  (E_15*E_2 - E_14*E_3)*E_4 - (E_0*E_15 - E_12*E_3)*E_6 + (E_0*E_14 - E_12*E_2)*E_7,
+  E_0*E_11*E_6 - E_0*E_10*E_7 - (E_11*E_2 - E_10*E_3)*E_4 - (E_3*E_6 - E_2*E_7)*E_8],
+ [-E_11*E_13*E_4 + E_11*E_12*E_5 - (E_15*E_5 - E_13*E_7)*E_8 + (E_15*E_4 - E_12*E_7)*E_9,
+  -E_1*E_11*E_12 + E_0*E_11*E_13 + (E_1*E_15 - E_13*E_3)*E_8 - (E_0*E_15 - E_12*E_3)*E_9,
+  -(E_1*E_15 - E_13*E_3)*E_4 + (E_0*E_15 - E_12*E_3)*E_5 + (E_1*E_12 - E_0*E_13)*E_7,
+  E_1*E_11*E_4 - E_0*E_11*E_5 + (E_3*E_5 - E_1*E_7)*E_8 - (E_3*E_4 - E_0*E_7)*E_9],
+ [E_10*E_13*E_4 - E_10*E_12*E_5 + (E_14*E_5 - E_13*E_6)*E_8 - (E_14*E_4 - E_12*E_6)*E_9,
+  E_1*E_10*E_12 - E_0*E_10*E_13 - (E_1*E_14 - E_13*E_2)*E_8 + (E_0*E_14 - E_12*E_2)*E_9,
+  (E_1*E_14 - E_13*E_2)*E_4 - (E_0*E_14 - E_12*E_2)*E_5 - (E_1*E_12 - E_0*E_13)*E_6,
+  -E_1*E_10*E_4 + E_0*E_10*E_5 - (E_2*E_5 - E_1*E_6)*E_8 + (E_2*E_4 - E_0*E_6)*E_9]]).
+
 
 my_sumlist([], 0).
 my_sumlist([H|T], N) :- my_sumlist(T, X), N #= X + H. 
@@ -55,11 +107,31 @@ my_sumlist([H|T], N) :- my_sumlist(T, X), N #= X + H.
 mmult(M1, M2, M3) :- transpose(M2,MT), maplist(mm_helper(MT), M1, M3).
 mm_helper(M2, I1, M3) :- maplist(dot(I1), M2, M3).
 
+not_equal([A1, A2, A3, A4],[B1, B2, B3, B4]):-
+    #\ ((A1 #= B1) #/\
+        (A2 #= B2) #/\
+	(A3 #= B3) #/\
+        (A4 #= B4) ).
 
-problem(1, A, B):-
+matrix_does_not_contain([], _).
+matrix_does_not_contain([H|T], R):-
+	not_equal(H, R),
+	matrix_does_not_contain(T, R).
+
+naive_full_rank([[_, _, _, _]]).
+naive_full_rank([H|T]):-
+	naive_full_rank(T),
+	matrix_does_not_contain(T, H).
+
+problem(1, A, B, C, ABf):-
     nn(NN),
-    isMat(A, NN),
+    smolMat(A, NN),
     isMat(B, NN),
+    %naive_full_rank(A),
+    %transpose(A, AT),
+    %naive_full_rank(AT),
     my_scamul(10, A, AX),
     addmat(AX, B, C),
-    mmult(A, B, C).
+    mmult(A, B, C),
+    append(A, B, AB),
+    flatten(AB, ABf).
